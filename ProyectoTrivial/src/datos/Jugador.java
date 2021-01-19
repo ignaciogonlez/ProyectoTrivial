f3b54d2a9f51ed050a2b3dccc9d2c7cb950d4d47
ignac <ignac@LAPTOP-NAT9P5FB>
2021-01-19 22:13:47 +0100

    Clase jugador y tablero en el que ya se ven los jugadores.

---
+++ ProyectoTrivial/src/datos/Jugador.java
@@ -0,0 +1,49 @@
+package datos;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class Jugador{
+	
+	private float x;
+	private float y;
+	private Rectangle rect;
+	private Color c;
+	
+	public Jugador(int x, int y, Color c) {
+		
+		this.c = c;
+		this.x = x;
+		this.y = y;
+		
+        rect = new Rectangle(x, y, 30, 30);
+    
+    }
+	
+	public float getx() {
+    	return x;
+    }
+    
+    public void setx(float x) {
+    	this.x = x;
+    }
+    
+    public float gety() {
+    	return y;
+    }
+    
+    public void set(float x) {
+    	this.x = x;
+    }
+     
+	public void paint(Graphics g) {
+	     g.setColor(c);
+	     g.fillRect(rect.x, rect.y, rect.width, rect.height);
+	}
+	       
+}
--- ProyectoTrivial/src/datos/Tablero.java
+++ ProyectoTrivial/src/datos/Tablero.java
@@ -1,8 +1,46 @@
 package datos;
 
-public class Tablero {
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class Tablero extends JFrame{
+	
+	Jugador j1;
+	Jugador j2;
+	Jugador j3;
+	Jugador j4;
 	
 	public Tablero() {
+		setSize(800, 600);
+		setTitle("Tablero de juego");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		j1 = new Jugador(500, 100, Color.RED);
+		j2 = new Jugador(600, 100, Color.DARK_GRAY);
+		j3 = new Jugador(400, 300, Color.BLUE);
+		j4 = new Jugador(700, 400, Color.YELLOW);
 		
 	}
+	
+	public void paint(Graphics g) {
+	     super.paint(g);
+	     
+	     j1.paint(g);
+	     j2.paint(g);
+	     j3.paint(g);
+	     j4.paint(g);
+	     
+	     g.dispose();
+	     
+	     
+	}
+	
+	public static void main(String[] args) {
+		Tablero t = new Tablero();
+	}
+	
 }
